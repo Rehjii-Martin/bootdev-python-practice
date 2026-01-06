f7b0c0d30b38406a04269421dae2a2892ec182a2
Rehjii Martin <rehjiimartin15@gmail.com>
2026-01-06 12:08:03 -0800

    chore: daily update [bootdev-python-practice] 2026-01-06 12:08:02

--- lists/counting_items_in_list.py
+++ lists/counting_items_in_list.py
@@ -6,3 +6,16 @@ Within the loop, check if the items are a Potion, Bread, or Shortsword.
 If you find a match, increment the corresponding counter, either potion_count, bread_count or shortsword_count. """
 
 
+def get_item_counts(items):
+    potion_count = 0
+    bread_count = 0
+    shortsword_count = 0
+
+    # don't touch above this line
+
+    for i in range(0, len(items)):
+        pass
+
+    # don't touch below this line
+
+    return potion_count, bread_count, shortsword_count
