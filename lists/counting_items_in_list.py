940e318710332770f265f975dc905d8c2005a307
Rehjii Martin <rehjiimartin15@gmail.com>
2026-01-06 12:25:21 -0800

    chore: daily update [bootdev-python-practice] 2026-01-06 12:25:21

--- lists/counting_items_in_list.py
+++ lists/counting_items_in_list.py
@@ -1,10 +1,27 @@
-""" Our players need a way to see how many copies of a specific item they have within their inventory!
+# Problem
 
-Let's finish the get_item_counts function.
+# Our players need a way to see how many copies of a specific item they have within their inventory!
+# Let's finish the get_item_counts function.
+# Within the loop, check if the items are a Potion, Bread, or Shortsword.
+# If you find a match, increment the corresponding counter, either potion_count, bread_count or shortsword_count. """
 
-Within the loop, check if the items are a Potion, Bread, or Shortsword.
-If you find a match, increment the corresponding counter, either potion_count, bread_count or shortsword_count. """
 
+'''
+Boot.dev – Python Track
+CH9: Lists
+Problem: Counting Items in a List
+
+Description:
+    Use a loop to count items in a list by enumerating a counter based on the
+    presence of an item within the list.
+
+Notes:
+    - if statements do not need else-blocks at all, they can terminate on completion
+    - returning is not necessary within loop
+
+Date: 2026-01-06
+Source: Boot.dev Training Grounds
+'''
 
 def get_item_counts(items):
     potion_count = 0
