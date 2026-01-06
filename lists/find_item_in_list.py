e06a1be0267ddb0841cbc551506542375754e411
Rehjii Martin <rehjiimartin15@gmail.com>
2026-01-06 13:02:49 -0800

    chore: daily update [bootdev-python-practice] 2026-01-06 13:02:48

---
+++ lists/find_item_in_list.py
@@ -0,0 +1,34 @@
+'''
+Boot.dev – Python Track
+CH9: Lists
+Problem: Finding Items in a List
+
+Description:
+    Use no-index or no-range syntax to find items within list.
+
+Notes:
+    -
+    -
+Date: 2026-01-06
+Source: Boot.dev Learn to Code in Python/Find an Item in a List
+'''
+
+# Problem
+# Practice the "no-index" or "no-range" syntax:
+    # for fruit in fruits:
+    #     print(fruit)
+# Assignment
+# We need to check if a player has a specific item in their inventory. 
+# In the contains_leather_scraps function, use the no-index syntax to iterate over each item in items. 
+# If you find an item called Leather Scraps, set the found variable to True.
+
+def contains_leather_scraps(items):
+    found = False
+
+    # don't touch above this line
+
+    # ?
+
+    # don't touch below this line
+
+    return found
